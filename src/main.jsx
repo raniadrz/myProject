8f80d9b5db20525c9b50eceab2e039a24fcaaca6
Ourania Ntrizi <drizirania@gmail.com>
2024-11-12 23:46:25 +0200

    inject on main for data vercel

--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,18 @@ import { ThemeProvider } from "@material-tailwind/react";
 import { Provider } from "react-redux";
 import { store } from './redux/store';
 
+// Analytics Vercel
+import { inject } from '@vercel/analytics';
 
-
+// Call inject() outside the render method
+inject();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-  
         <ThemeProvider>
           <App />
+          
       
         </ThemeProvider>
 
